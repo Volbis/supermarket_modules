0c11c397a3b59b985f8c95c623ee208fb4454f00
Yehezkelou <Tokapieuoez@gmail.com>
2025-10-23 12:42:43 +0000

    modif notification

--- backend/supermarket/gestion_stocks/apps.py
+++ backend/supermarket/gestion_stocks/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class GestionStocksConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'gestion_stocks'
+    
+    def ready(self):
+        """Importer les signals au démarrage de l'application."""
+        import gestion_stocks.signals
